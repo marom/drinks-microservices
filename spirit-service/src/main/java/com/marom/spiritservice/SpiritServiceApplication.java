f45e27b13b7260eed5cf404c107f5c5ddb50ae5c
marom <maro.muszynski@gmail.com>
2019-05-02 11:02:50 +0200

    initial commit

---
+++ spirit-service/src/main/java/com/marom/spiritservice/SpiritServiceApplication.java
@@ -0,0 +1,13 @@
+package com.marom.spiritservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpiritServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpiritServiceApplication.class, args);
+    }
+
+}

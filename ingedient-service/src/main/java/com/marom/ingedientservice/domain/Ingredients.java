5066c6f1439635de3d4104bbcf17e58ea9568ef9
marom <maro.muszynski@gmail.com>
2019-05-02 11:27:49 +0200

    add domain objects

---
+++ ingedient-service/src/main/java/com/marom/ingedientservice/domain/Ingredients.java
@@ -0,0 +1,21 @@
+package com.marom.ingedientservice.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Ingredients {
+
+    private List<Ingredient> ingredients = new ArrayList<>();
+
+    public Ingredients(List<Ingredient> ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public List<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(List<Ingredient> ingredients) {
+        this.ingredients = ingredients;
+    }
+}

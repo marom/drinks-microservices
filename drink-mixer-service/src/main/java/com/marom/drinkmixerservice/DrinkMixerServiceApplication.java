ca6b57afca291dcf6d1095a29f624583ef83de9d
marom <maro.muszynski@gmail.com>
2019-05-02 11:40:59 +0200

    initial commit for drink mixer service module

---
+++ drink-mixer-service/src/main/java/com/marom/drinkmixerservice/DrinkMixerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.marom.drinkmixerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrinkMixerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DrinkMixerServiceApplication.class, args);
+    }
+
+}
---
+++ drink-mixer-service/src/test/java/com/marom/drinkmixerservice/DrinkMixerServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.marom.drinkmixerservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DrinkMixerServiceApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}

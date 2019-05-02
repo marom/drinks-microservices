a09e63d8645035986e2cffbc19956897c7bdfeb5
marom <maro.muszynski@gmail.com>
2019-05-02 12:57:40 +0200

    enable Eureka client

--- drink-mixer-service/src/main/java/com/marom/drinkmixerservice/DrinkMixerServiceApplication.java
+++ drink-mixer-service/src/main/java/com/marom/drinkmixerservice/DrinkMixerServiceApplication.java
@@ -2,9 +2,12 @@ package com.marom.drinkmixerservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class DrinkMixerServiceApplication {
 
@@ -13,7 +16,7 @@ public class DrinkMixerServiceApplication {
     }
 
 
-
+    @LoadBalanced
     @Bean
     public RestTemplate getRestTemplate() {
         return new RestTemplate();

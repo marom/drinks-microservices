ebc453f1fe1eb3bcb8d8ac71cef4d0cc54655211
marom <maro.muszynski@gmail.com>
2019-05-02 11:25:14 +0200

    initial commit for ingredient service module

---
+++ ingedient-service/src/main/java/com/marom/ingedientservice/IngedientServiceApplication.java
@@ -0,0 +1,13 @@
+package com.marom.ingedientservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IngedientServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IngedientServiceApplication.class, args);
+    }
+
+}
---
+++ ingedient-service/src/test/java/com/marom/ingedientservice/IngedientServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.marom.ingedientservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IngedientServiceApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}

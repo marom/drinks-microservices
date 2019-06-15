e97e1c91daf200ddeeef5236c2b11bc8b00e15b8
marom <maro.muszynski@gmail.com>
2019-06-15 18:08:07 +0100

    add Swagger2 to the project

--- drink-mixer-service/src/main/java/com/marom/drinkmixerservice/DrinkMixerServiceApplication.java
+++ drink-mixer-service/src/main/java/com/marom/drinkmixerservice/DrinkMixerServiceApplication.java
@@ -1,20 +1,33 @@
 package com.marom.drinkmixerservice;
 
+import org.apache.maven.model.Model;
+import org.apache.maven.model.io.xpp3.MavenXpp3Reader;
+import org.codehaus.plexus.util.xml.pull.XmlPullParserException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.io.FileReader;
+import java.io.IOException;
 
 @EnableEurekaClient
 @SpringBootApplication
+@EnableSwagger2
 public class DrinkMixerServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DrinkMixerServiceApplication.class, args);
     }
-
 
     @LoadBalanced
     @Bean
@@ -22,4 +35,17 @@ public class DrinkMixerServiceApplication {
         return new RestTemplate();
     }
 
+    @Bean
+    public Docket api() throws IOException, XmlPullParserException {
+        MavenXpp3Reader reader = new MavenXpp3Reader();
+        Model model = reader.read(new FileReader("pom.xml"));
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.marom.drinkmixerservice.controller"))
+                .paths(PathSelectors.any())
+                .build().apiInfo(new ApiInfo("Drink Mixer Service Api Documentation", "Documentation automatically generated",
+                        model.getParent().getVersion(),
+                        null,
+                        new Contact("MaroM", "webpage.com", "marom@mail.com"), null, null));
+    }
 }

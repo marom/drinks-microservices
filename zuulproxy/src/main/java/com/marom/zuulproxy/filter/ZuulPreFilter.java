83bc56eee4777e36e02bf399f2ece5da0cdbf457
marom <maro.muszynski@gmail.com>
2019-06-15 10:30:57 +0100

    add pre filter to log request method and request url

---
+++ zuulproxy/src/main/java/com/marom/zuulproxy/filter/ZuulPreFilter.java
@@ -0,0 +1,39 @@
+package com.marom.zuulproxy.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Component
+@Slf4j
+public class ZuulPreFilter extends ZuulFilter {
+
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 1;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest request = ctx.getRequest();
+
+        log.info(String.format("%s request to %s", request.getMethod(), request.getRequestURL().toString()));
+        return null;
+    }
+}
